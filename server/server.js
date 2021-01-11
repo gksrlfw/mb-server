d611c990cea1c938c6351c776456f08c8eb8727e
gksrlfw <gksrlfw@naver.com>
2021-01-11 15:04:04 +0900

    last test

--- server/server.js
+++ server/server.js
@@ -17,12 +17,12 @@ dotenv.config();
 passportConfig();
 
 console.log('dotenv:', process.env.PORT, process.env.PROD_HOST, process.env.PROD_NAME, process.env.JWT_SECRET, process.env.MAIL_EMAIL, process.env.COOKIE_SECRET);
-checkDir('/public/images');
-checkDir('/public/videos');
-checkDir('/public/images/profiles');
-checkDir('/public/images/lessons');
-checkDir('/public/videos/lessons');
-checkDir('/public/videos/m3u8');
+// checkDir('/public/images');
+// checkDir('/public/videos');
+// checkDir('/public/images/profiles');
+// checkDir('/public/images/lessons');
+// checkDir('/public/videos/lessons');
+// checkDir('/public/videos/m3u8');
 
 const indexRouter = require('./routes');
 
